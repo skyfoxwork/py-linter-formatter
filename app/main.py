f0cf009b4ffe90884189444e42f5eac0be608d9f
roman <skyfoxwork@gmail.com>
2024-09-08 17:21:50 +0300

    format_single_linter_file v 2.0 with pytest and flake8

--- app/main.py
+++ app/main.py
@@ -8,33 +8,15 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {("errors" if i == "error" else i) : ([format_linter_error(error) for error in errors] if i == "error" else file_path if i == "path" else "failed" if i == "status" else "")for i in ("error", "path", "status")}
+    return {
+        ("errors" if elem == "error" else elem):
+        ([format_linter_error(error) for error in errors]
+         if elem == "error" else file_path if elem == "path" else
+         "failed" if elem == "status" else "")
+        for elem in ("error", "path", "status")
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
     return None
-
-
-errors = [
-    {
-        "code": "E501",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 80,
-        "text": "line too long (99 > 79 characters)",
-        "physical_line": '    return f"I like to filter, rounding, doubling, '
-        "store and decorate numbers: {', '.join(items)}!\"",
-    },
-    {
-        "code": "W292",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 100,
-        "text": "no newline at end of file",
-        "physical_line": '    return f"I like to filter, rounding, doubling, '
-        "store and decorate numbers: {', '.join(items)}!\"",
-    },
-]
-
-print(format_single_linter_file(file_path="./source_code_2.py", errors=errors))
